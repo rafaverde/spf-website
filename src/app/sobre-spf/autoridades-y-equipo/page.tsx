2d453b32aeebb1826a206c16b22132200bf08450
Rafael Valverde <rafaverde@msn.com>
2025-12-19 18:22:42 -0300

    feat: create Authority and Team page, Authority card and members list component and configuration.

--- src/app/sobre-spf/autoridades-y-equipo/page.tsx
+++ src/app/sobre-spf/autoridades-y-equipo/page.tsx
@@ -1,9 +1,78 @@
 import HeroImageBackground from "@/components/layout/hero-image-background";
+import HeroTitle from "@/components/layout/hero-title";
 
-export default function TeamPage() {
+import { authorityConfig } from "@/lib/site/authority-team.config";
+import AuthorityCard from "./components/authority-card";
+import AuthorityMembersList from "./components/authority-members-list";
+
+export default function AuthorityTeamPage() {
+  const { titularMembers } = authorityConfig;
+
   return (
     <div className="relative min-h-screen w-full">
       <HeroImageBackground imageSrc="/about-us/team/bg-header-team.webp" />
+      <div className="relative z-10 flex flex-col">
+        <HeroTitle staticTitle="Autoridades y Equipo" titlePosition="end" />
+
+        <section className="bg-white py-20">
+          <div className="text-spf-green-900 container mx-auto space-y-11 px-4 lg:px-0">
+            <div className="space-y-4">
+              <p>
+                La Sociedad de Productores Forestales es dirigida y gestionada
+                por una Comisión Directiva, electa por los socios cada dos años.
+              </p>
+              <h3 className="text-spf-green-500 text-4xl">
+                Miembros Titulares
+              </h3>
+            </div>
+
+            {/* Presidencia */}
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              {titularMembers.presidency.map((member, index) => (
+                <AuthorityCard
+                  key={index}
+                  name={member.name}
+                  imageSrc={member.imageSrc}
+                  title={member.title}
+                />
+              ))}
+            </div>
+
+            {/* Membros */}
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+              <AuthorityMembersList
+                sectionTitle={titularMembers.secretary.sectionTitle}
+                names={titularMembers.secretary.members}
+              />
+
+              <AuthorityMembersList
+                sectionTitle={titularMembers.treasury.sectionTitle}
+                names={titularMembers.treasury.members}
+                className="col-span-2"
+              />
+
+              <AuthorityMembersList
+                sectionTitle={titularMembers.vowels.sectionTitle}
+                names={titularMembers.vowels.members}
+              />
+
+              <AuthorityMembersList
+                sectionTitle={titularMembers.alternates.sectionTitle}
+                names={titularMembers.alternates.members}
+              />
+
+              <AuthorityMembersList
+                sectionTitle={titularMembers.fiscalCommition.sectionTitle}
+                names={titularMembers.fiscalCommition.members}
+                subSectionTitle={titularMembers.fiscalCommition.subsectionTitle}
+                subSectionNames={
+                  titularMembers.fiscalCommition.subSectionMembers
+                }
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/lib/site/authority-team.config.ts
@@ -0,0 +1,72 @@
+export const authorityConfig = {
+  titularMembers: {
+    presidency: [
+      {
+        imageSrc: "/about-us/team/team-lucia-basso.webp",
+        name: "Lucia Basso",
+        title: "Presidente",
+      },
+      {
+        imageSrc: "/about-us/team/team-francisco-bonino.webp",
+        name: "Francisco Bonino",
+        title: "Vice-Presidente",
+      },
+    ],
+    secretary: {
+      sectionTitle: "Secretario",
+      members: [{ name: "Alberto Rodriguez" }],
+    },
+    treasury: {
+      sectionTitle: "Tesorero",
+      members: [{ name: "Pablo Balerio" }],
+    },
+    vowels: {
+      sectionTitle: "Vocales",
+      members: [
+        { name: "Javier Otegui" },
+        { name: "Diego Mora" },
+        { name: "Álvaro Fitipaldo" },
+        { name: "Diego Carrau" },
+        { name: "Sebastián O’Brien" },
+        { name: "Álvaro Molinari" },
+        { name: "Mauricio Berton" },
+        { name: "Alberto Voulminot" },
+        { name: "Nelson Ledesma" },
+        { name: "Daniel Ramírez" },
+      ],
+    },
+    alternates: {
+      sectionTitle: "Suplentes",
+      members: [
+        { name: "Inés Arocena" },
+        { name: "Dan Guapura" },
+        { name: "Alina Gil" },
+        { name: "Thomas Coubrough" },
+        { name: "Juan Manuel Otegui" },
+        { name: "Gerardo Barrios" },
+        { name: "Gonzalo Giambruno" },
+        { name: "María Noel Fadel" },
+        { name: "Juan José Ferber" },
+        { name: "Ricardo Inciarte" },
+        { name: "Carlos Sarazola" },
+        { name: "Luis Gomes" },
+        { name: "Alejandro Venturino" },
+        { name: "Laura Martignone" },
+      ],
+    },
+    fiscalCommition: {
+      sectionTitle: "Comisión Fiscal",
+      members: [
+        { name: "Leonardo Dotti" },
+        { name: "Andrea Regusci" },
+        { name: "Bernardo Riet" },
+      ],
+      subsectionTitle: "Suplentes",
+      subSectionMembers: [
+        { name: "Enrique Galli" },
+        { name: "Lucas Máscolo" },
+        { name: "Rosario Linares" },
+      ],
+    },
+  },
+};
