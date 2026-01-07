0e8acdd35e8bdf4fde1ca26501488403c57f8af4
Rafael Valverde <rafaverde@msn.com>
2026-01-07 18:33:32 -0300

    fix(macondo-signature): adjust layout for better responsiveness and structure

--- src/components/layout/macondo-signature.tsx
+++ src/components/layout/macondo-signature.tsx
@@ -10,33 +10,35 @@ export default function MacondoSignature({ className }: MacondoSignatureProps) {
   return (
     <section className={cn("w-full bg-zinc-900 py-4 text-zinc-100", className)}>
       <div className="container mx-auto">
-        <div className="mx-auto flex w-fit items-center justify-center gap-3">
+        <div className="mx-auto flex w-fit flex-col items-center justify-center gap-3 md:flex-row">
           <p className="text-xs leading-0">Desarrollado por</p>
-          <Link
-            href="https://www.macondo.com.uy"
-            target="_blank"
-            className="-mt-1 transition-opacity duration-500 ease-in-out hover:opacity-70"
-          >
-            <Image
-              src="/logo-macondo-marketing.svg"
-              alt="Macondo Marketing y Comunicación"
-              width={112}
-              height={20}
-            />
-          </Link>
-          <p className="text-xs leading-0">/</p>
-          <Link
-            href="https://rakhiestudio.com/"
-            target="_blank"
-            className="transition-opacity duration-500 ease-in-out hover:opacity-70"
-          >
-            <Image
-              src="/rakhi-marca-negativa.svg"
-              alt="Rakhi Comunicación y Marketing Digital"
-              width={80}
-              height={18}
-            />
-          </Link>
+          <div className="flex items-center justify-center gap-3">
+            <Link
+              href="https://www.macondo.com.uy"
+              target="_blank"
+              className="-mt-1 transition-opacity duration-500 ease-in-out hover:opacity-70"
+            >
+              <Image
+                src="/logo-macondo-marketing.svg"
+                alt="Macondo Marketing y Comunicación"
+                width={112}
+                height={20}
+              />
+            </Link>
+            <p className="text-xs leading-0">/</p>
+            <Link
+              href="https://rakhiestudio.com/"
+              target="_blank"
+              className="transition-opacity duration-500 ease-in-out hover:opacity-70"
+            >
+              <Image
+                src="/rakhi-marca-negativa.svg"
+                alt="Rakhi Comunicación y Marketing Digital"
+                width={80}
+                height={18}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
