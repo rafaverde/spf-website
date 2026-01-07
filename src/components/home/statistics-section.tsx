bc18829a09bd85f72eaaa1244a62023fdb4e72c4
Rafael Valverde <rafaverde@msn.com>
2026-01-06 23:10:46 -0300

    fix(statistics-section): add table for agro surface data and adjust layout

--- src/components/home/statistics-section.tsx
+++ src/components/home/statistics-section.tsx
@@ -3,6 +3,22 @@ import { Button } from "../ui/button";
 import { RiArrowRightUpLine } from "@remixicon/react";
 import StatisticsCounter from "../statistics/statistics-counter";
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+const yearsAgroSurface = [
+  { year: 1990, value: 178.925 },
+  { year: 2000, value: 598.891 },
+  { year: 2010, value: 870.424 },
+  { year: 2020, value: 1087.109 },
+  { year: 2020, value: 1161.851 },
+];
 
 export default function StatisticsSection() {
   return (
@@ -19,19 +35,54 @@ export default function StatisticsSection() {
         <h2 className="text-spf-green-100 text-4xl">El sector en cifras</h2>
 
         <div className="grid grid-cols-1 lg:grid-cols-3">
-          <StatisticsCounter
-            value={6.6}
-            sufix="%"
-            title="Áreas forestadas"
-            decimals={1}
-          />
-          <StatisticsCounter value={46000} title="Puestos de trabajo" />
-          <StatisticsCounter
-            value={3000}
-            sufix="M"
-            prefix="US$"
-            title="Exportaciones"
-          />
+          <div className="flex flex-col items-center justify-center">
+            <StatisticsCounter
+              value={6.6}
+              sufix="%"
+              title="Superficie Agropecuaria"
+              decimals={1}
+            />
+
+            <div>
+              <Table className="border">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="text-spf-green-300 text-center">
+                      Año
+                    </TableHead>
+                    <TableHead className="text-spf-green-300 text-center">
+                      Total Acumulado (ha)
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {yearsAgroSurface.map((item) => (
+                    <TableRow key={item.year}>
+                      <TableCell className="text-spf-green-100 text-center">
+                        {item.year}
+                      </TableCell>
+                      <TableCell className="text-spf-green-100 text-center">
+                        {item.value}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </div>
+
+          <div>
+            <StatisticsCounter value={46000} title="Puestos de trabajo" />
+          </div>
+
+          <div>
+            <StatisticsCounter
+              value={3000}
+              sufix="M"
+              prefix="US$"
+              title="Exportaciones"
+            />
+          </div>
         </div>
 
         <div className="text-right">
