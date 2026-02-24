ab23596551a60142b6ab57f5e769c428182f2dd3
Rafael Valverde <rafaverde@msn.com>
2026-02-24 10:42:13 -0300

    fix(utils): improve date formatting in formatDate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { WpPost } from "./wp/wp.types";
-import { NewsItem } from "./news/news.types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,7 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 // Formata a data para padrão es-UY
 export function formatDate(date: string) {
-  const formattedDate = new Date(date).toLocaleDateString("es-UY", {
+  const convertedDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`;
+
+  const formattedDate = new Date(convertedDate).toLocaleDateString("es-UY", {
     day: "numeric",
     month: "long",
     year: "numeric",
