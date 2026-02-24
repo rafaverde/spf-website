21c74e13c9aed27fd546930be8f72a19fee06149
Rafael Valverde <rafaverde@msn.com>
2026-02-24 11:40:16 -0300

    fix(utils): enhance date conversion logic in formatDate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 // Formata a data para padrão es-UY
 export function formatDate(date: string) {
-  const convertedDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`;
+  const convertedDate =
+    date.length === 8
+      ? `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`
+      : date;
 
   const formattedDate = new Date(convertedDate).toLocaleDateString("es-UY", {
     day: "numeric",
